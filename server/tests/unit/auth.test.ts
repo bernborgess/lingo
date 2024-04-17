7883369a1c8b9065b6303b0df5181e8e5d37a913
RdgFerreira <araujofro@gmail.com>
2024-04-17 18:51:04 -0300

    Added tests to verify from auth

--- server/tests/unit/auth.test.ts
+++ server/tests/unit/auth.test.ts
@@ -1,12 +1,14 @@
 import jwt from "jsonwebtoken";
-import { sign } from "../../src/middlewares/auth";
+import { sign, verify, isLoggedIn, JwtData } from "../../src/middlewares/auth";
+import { Request, Response, NextFunction } from "express";
 
 jest.mock("jsonwebtoken", () => ({
-    sign: jest.fn()
+    sign: jest.fn(),
+    verify: jest.fn(),
+    isLoggedIn: jest.fn(),
 }));
 
-
-describe("sign works correctly", () => {
+describe("`sign` works correctly", () => {
     let originalEnv: NodeJS.ProcessEnv;
 
     beforeEach(() => {
@@ -42,7 +44,52 @@ describe("sign works correctly", () => {
 })
 
 // TODO
-describe("verify can read the token", () => { })
+describe("`verify` can read the token", () => {
+    let originalEnv: NodeJS.ProcessEnv;
+
+    beforeEach(() => {
+        originalEnv = process.env;
+    })
+
+    afterEach(() => {
+        process.env = originalEnv;
+        jest.clearAllMocks();
+    })
+
+    it("Fails when JWT_SECRET is not defined", () => {
+        delete process.env.JWT_SECRET;
+        expect(verify("testToken")).toBe("JWT_SECRET_NOT_FOUND");
+    })
+
+    it("Fails when decoded token is a string", () => {
+        process.env.JWT_SECRET = "testSecret";
+        jwt.verify = jest.fn().mockReturnValue("testDecoded");
+        expect(verify("testToken")).toBe("INVALID_TOKEN");
+    })
+
+    it("Fails when jwt.verify throws an exception", () => {
+        process.env.JWT_SECRET = "testSecret";
+        jwt.verify = jest.fn().mockImplementation(() => {
+            throw new Error();
+        });
+        expect(verify("testToken")).toBe("INVALID_TOKEN");
+    })
+
+    it("Works when token is valid", () => {
+        process.env.JWT_SECRET = "testSecret";
+        const testJwtData : JwtData = {id: "testId", email: "testEmail@email.com"};
+        jwt.verify = jest.fn().mockImplementation(() => testJwtData);
+        expect(verify("testToken")).toBe(testJwtData);
+    })
+
+})
 
 // TODO
-describe("isLoggedIn middleware allows only logged users", () => { })
+describe("isLoggedIn middleware allows only logged users", () => {
+    it("Fails when there is no token", () => {
+        let testReq: any = undefined;
+        let testRes: any = undefined;
+        let testNext: any = undefined;
+        // expect(isLoggedIn(testReq, testRes, testNext)).toBe(Response);
+    })
+})
