db338e6d7ff899bde9c0fe2f68051246fe08c585
bernborgess <bernborgess@outlook.com>
2024-04-25 10:47:19 -0300

    Add `getStatement` tests

--- server/tests/unit/questionService.test.ts
+++ server/tests/unit/questionService.test.ts
@@ -47,6 +47,63 @@ describe("getStatement returns a valid statement", () => {
         expect(question).toMatchObject(statement);
     });
 
+    it("Denies user that did not reach the level", async () => {
+        const user: User = {
+            id: "userid",
+            email: "user@email.com",
+            username: "username",
+            currentLevel: 1
+        };
+
+        userService.getUserById = jest.fn().mockResolvedValue(user);
+
+        await expect(questionService.getStatement(2, 1, "userid"))
+            .rejects
+            .toThrow("User did not reach this level");
+    });
+
+    it("Throws an error when the question does not exist", async () => {
+        const user: User = {
+            id: "userid",
+            email: "user@email.com",
+            username: "username",
+            currentLevel: 1
+        };
+
+        userService.getUserById = jest.fn().mockResolvedValue(user);
+        prisma.question.findFirst = jest.fn().mockResolvedValue(null);
+
+        await expect(questionService.getStatement(1, 1, "userid"))
+            .rejects
+            .toThrow("Question not found!");
+    });
+
+    it("Allows an user that reached the level", async () => {
+        const question: QuestionDB = {
+            id: '66297573db145161e46c203b',
+            levelSequence: 2,
+            sequence: 1,
+            type: 'Ordering',
+            phrase: 'Você é um gato?',
+            options: ['Are', 'you', 'a', 'cat', 'horse', 'girl'],
+            answer: ['Are', 'you', 'a', 'cat'],
+            answerId: 0
+        };
+
+        const user: User = {
+            id: "userid",
+            email: "user@email.com",
+            username: "username",
+            currentLevel: 2
+        };
+
+        prisma.question.findFirst = jest.fn().mockResolvedValue(question);
+        userService.getUserById = jest.fn().mockResolvedValue(user);
+
+        const statement = await questionService.getStatement(2, 1, "userid");
+
+        expect(question).toMatchObject(statement);
+    })
 
 
 })
