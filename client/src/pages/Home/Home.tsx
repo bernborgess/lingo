a87b622cfbdb8504eae37ae8b07d7acc850e426f
Ga-Alves <gabrielalvesreis@gmail.com>
2024-05-20 19:27:32 -0300

    feat: home page ( level page)

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,16 +1,24 @@
 // import { useUserData } from "../../utils/context/AuthContext";
 import LevelButton from '../../utils/components/LevelButton/LevelButton'
 import './Home.css'
+
+import LingoPointLeft from '../../assets/LingoPointLeft.svg'
 export default function Home() {
     // const { userData } = useUserData();
 
     // console.log(userData);
+    function handleSelectLevel(id:number) {
+        console.log('Level'+ id)
+    }
+
+    const levels = [2,3,4,5]
     return (
         <div className='Home'>
-            <LevelButton/>
-            <LevelButton enable={false}/>
-            <LevelButton enable={false}/>
-            <LevelButton enable={false}/>
+            <div className='Levels'>
+                <LevelButton onClick={() => handleSelectLevel(1)}/>
+                {levels.map((level) => <LevelButton enable={false} onClick={() => handleSelectLevel(level)}/>)}
+            </div>
+            <img src={LingoPointLeft} alt="LingoPointLeft" />
         </div>
     )
 }
