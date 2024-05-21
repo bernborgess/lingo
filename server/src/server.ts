2d72f8066086c7d48e04cb35e0df7cfa619b6ba4
bernborgess <bernborgess@outlook.com>
2024-05-21 18:45:19 -0300

    Change CORS rule

--- server/src/server.ts
+++ server/src/server.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(cors(
     {
-        origin: [process.env.APP_URL || "http://127.0.0.1:5173"],
+        origin: [process.env.APP_URL || "http://localhost:5173"],
         credentials: true
     }
 ));
