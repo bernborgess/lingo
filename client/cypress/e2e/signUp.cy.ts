f0b892f8092aad826f6f2d02560a7c1d9c0a36ae
Daniele-Cassia <danielecassia9@gmail.com>
2024-06-29 17:35:11 -0300

    e2e: muda o teste signUp para conter valores unicos

--- client/cypress/e2e/signUp.cy.ts
+++ client/cypress/e2e/signUp.cy.ts
@@ -5,13 +5,17 @@ describe('Criar Nova Conta', () => {
     
     it('Acessa tela de criação de conta e preenche formulário', () => {
     cy.contains('Sign Up').click()
+
+    const uniqueUser = `testeCypress${Date.now()}`
+    const uniqueEmail = `teste${Date.now()}@cypress.com`
+
     cy.get('[data-testID="form-input-user"]').should('exist')
     cy.get('[data-testID="form-input-email"]').should('exist')
     cy.get('[data-testID="form-input-password"]').should('exist')
     cy.wait(1000)
-    cy.get('[data-testID="form-input-user"]').type('testeCypress')
+    cy.get('[data-testID="form-input-user"]').type(uniqueUser)
     cy.wait(1000)
-    cy.get('[data-testID="form-input-email"]').type('teste@cypress.com')
+    cy.get('[data-testID="form-input-email"]').type(uniqueEmail)
     cy.wait(1000)
     cy.get('[data-testID="form-input-password"]').type('cypress@teste')
     cy.wait(1000)
@@ -22,7 +26,7 @@ describe('Criar Nova Conta', () => {
     cy.get('[data-testID="form-input-user"]').should('exist')
     cy.get('[data-testID="form-input-password"]').should('exist')
     cy.wait(500)
-    cy.get('[data-testID="form-input-user"]').type('testeCypress')
+    cy.get('[data-testID="form-input-user"]').type(uniqueUser)
     cy.wait(500)
     
     cy.get('[data-testID="form-input-password"]').type('cypress@teste')
