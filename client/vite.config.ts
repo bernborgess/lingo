42942c3490af2ebb062c74d56ffda92c77d301eb
bernborgess <bernborgess@outlook.com>
2024-06-29 20:37:13 +0000

    Coverage working (istanbul)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     plugins: [react({
         babel: {
-            babelrc: true
+            plugins: ["istanbul"]
         }
     })],
     server: {
