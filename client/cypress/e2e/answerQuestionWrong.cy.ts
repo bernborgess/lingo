81373dacb62772524a3036a061d4633dab9eee0b
bernborgess <bernborgess@outlook.com>
2024-06-29 21:43:52 +0000

    Change e2e test to fail (wrong answer)

--- client/cypress/e2e/answerQuestionWrong.cy.ts
+++ client/cypress/e2e/answerQuestionWrong.cy.ts
@@ -1,32 +1,32 @@
 describe('Respondendo a questão de forma errada', () => {
-  beforeEach(()=>{
-    cy.visit('http://localhost:5173/')
-    
-  });
-  
-  it('faz login e responde a questão', () => {
-    cy.get('[data-testID="form"]').should('exist')
-    cy.get('[data-testID="form-input"]').should('exist')
-    cy.get('[data-testID="form-input-user"]').should('exist')
-    cy.get('[data-testID="form-input-password"]').should('exist')
-    cy.wait(500)
-    cy.get('[data-testID="form-input-user"]').type('string')
-    cy.wait(500)
-    
-    cy.get('[data-testID="form-input-password"]').type('string')
-    cy.wait(500)
-    cy.get('[data-testID="button"]').click()
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/')
 
-    cy.get('[data-testID="card"]').should('exist')
-    cy.get('[data-testID="card-button-2"]').should('exist')
+    });
 
-    cy.get('[data-testID="card-button-2"]').click()
-    cy.get('[data-testID="word-1"]').click()
-    cy.get('[data-testID="word-2"]').click()
-    cy.get('[data-testID="word-3"]').click()
-    cy.get('[data-testID="word-4"]').click()
-    cy.get('[data-testID="button"]').click()
-    cy.contains('Wrong Answer!')
-  });
+    it('faz login e responde a questão', () => {
+        cy.get('[data-testID="form"]').should('exist')
+        cy.get('[data-testID="form-input"]').should('exist')
+        cy.get('[data-testID="form-input-user"]').should('exist')
+        cy.get('[data-testID="form-input-password"]').should('exist')
+        cy.wait(500)
+        cy.get('[data-testID="form-input-user"]').type('string')
+        cy.wait(500)
+
+        cy.get('[data-testID="form-input-password"]').type('string')
+        cy.wait(500)
+        cy.get('[data-testID="button"]').click()
+
+        cy.get('[data-testID="card"]').should('exist')
+        cy.get('[data-testID="card-button-2"]').should('exist')
+
+        cy.get('[data-testID="card-button-2"]').click()
+        cy.get('[data-testID="word-1"]').click()
+        cy.get('[data-testID="word-2"]').click()
+        cy.get('[data-testID="word-3"]').click()
+        cy.get('[data-testID="word-5"]').click()
+        cy.get('[data-testID="button"]').click()
+        cy.contains('Wrong Answer!')
+    });
 
 })
